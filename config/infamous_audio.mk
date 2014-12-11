735219241f4a8427a5744014bda8d252470c31c8
pio masaki <piomasaki@gmail.com>
2014-12-10 20:57:26 -0700

    Correct paths in infamous_audio.mk
    Change from cm to infamous

--- config/infamous_audio.mk
+++ config/infamous_audio.mk
@@ -2,9 +2,9 @@
 # CyanogenMod Audio Files
 #
 
-ALARM_PATH := vendor/cm/prebuilt/common/media/audio/alarms
-NOTIFICATION_PATH := vendor/cm/prebuilt/common/media/audio/notifications
-RINGTONE_PATH := vendor/cm/prebuilt/common/media/audio/ringtones
+ALARM_PATH := vendor/infamous/prebuilt/common/media/audio/alarms
+NOTIFICATION_PATH := vendor/infamous/prebuilt/common/media/audio/notifications
+RINGTONE_PATH := vendor/infamous/prebuilt/common/media/audio/ringtones
 UI_PATH := vendor/cm/prebuilt/common/media/audio/ui
 
 # Alarms
