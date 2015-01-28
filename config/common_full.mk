50113925d7aaba4620d567fc1bbf2dd91869dbaa
Michael Bestas <mikeioannina@gmail.com>
2015-01-28 02:04:48 +0200

    Remove deprecated VideoEditor packages
    Change-Id: I47b1d17e6b93dae6992fe169d034bfa4eb820370
    
    Conflicts:
    	config/common_full.mk

--- config/common_full.mk
+++ config/common_full.mk
@@ -1,9 +1,6 @@
 # Inherit common infamous stuff
 $(call inherit-product, vendor/infamous/config/common.mk)
 
-# Bring in all video files
-$(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
-
 # Include infamous audio files
 include vendor/infamous/config/infamous_audio.mk
 
@@ -23,14 +20,6 @@ PRODUCT_PACKAGES += \
     PhotoTable \
     SoundRecorder \
     PhotoPhase
-
-PRODUCT_PACKAGES += \
-    VideoEditor \
-    libvideoeditor_jni \
-    libvideoeditor_core \
-    libvideoeditor_osal \
-    libvideoeditor_videofilters \
-    libvideoeditorplayer
 
 # Extra tools in infamous
 PRODUCT_PACKAGES += \
